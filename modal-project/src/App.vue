3472e57781ee45aca2976a0f404f9aef97e64a26
kkoahis <khoakun27@gmail.com>
2023-12-06 16:39:14 +0700

    Vue CLI, file & template, Template refs, Multiple components

---
+++ modal-project/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <h1>{{ title }}</h1>
+  <input type="text" ref="name" />
+  <button @click="handleClick">Click me</button>
+  <Modal/>
+</template>
+
+<script>
+import Modal from "./components/Modal.vue";
+
+export default {
+  name: "App",
+  components: {
+    Modal,
+  },
+  data() {
+    return {
+      title: "My fisrt Vue App",
+    };
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name.value);
+      this.$refs.name.classList.add("active");
+      this.$refs.name.focus();
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1 {
+  color: rebeccapurple;
+  border-bottom: 1px solid #000;
+  display: inline-block;
+}
+</style>
---
+++ modal-project/src/components/Modal.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="backdrop">
+        <div class="modal">
+            <p>Modal content</p>
+        </div>
+    </div>
+</template>
+
+<style>
+    .modal{
+        width: 400px;
+        padding: 20px;
+        margin: 100px auto;
+        background: white;
+        border-radius: 10px;
+    }
+    .backdrop{
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: rgba(0,0,0,0.5);
+        z-index: 100;
+    }
+    /* .backdrop:target{
+        display: block;
+    } */
+    .backdrop:target .modal{
+        display: block;
+    }
+
+</style>
