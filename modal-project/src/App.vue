278821156e0474d91c86ca14f9ad50a4077c5368
kkoahis <khoakun27@gmail.com>
2023-12-06 23:16:05 +0700

    Props, Emitting custom events, Click event modifiers, Slots, Using Teleport

--- modal-project/src/App.vue
+++ modal-project/src/App.vue
@@ -1,8 +1,31 @@
 <template>
   <h1>{{ title }}</h1>
+  <br>
   <input type="text" ref="name" />
   <button @click="handleClick">Click me</button>
-  <Modal/>
+  <Teleport to=".modals" v-if="showModal">
+    <Modal theme="sale" @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">sign up now</a>
+        <br>
+        <a href="#">more info</a>
+      </template>
+      <h1>FB Give Away</h1>
+      <p>Grab your course with 0$</p>
+    </Modal>
+  </Teleport>
+
+
+  <div v-if="showModalTwo">
+    <Modal theme="" @close="toggleModalTwo">
+      <h1>Sign up to the newletters</h1>
+      <p>For update and promo codes!</p>
+    </Modal>
+  </div>
+
+  <br><br>
+  <button @click.alt="toggleModal">open Modal (alt + click)</button>
+  <button @click="toggleModalTwo">open Modal</button>
 </template>
 
 <script>
@@ -16,6 +39,10 @@ export default {
   data() {
     return {
       title: "My fisrt Vue App",
+      header: "Sign up for Give away!",
+      text: "Grab your tool with 0$",
+      showModal: false,
+      showModalTwo: true,
     };
   },
   methods: {
@@ -24,12 +51,18 @@ export default {
       this.$refs.name.classList.add("active");
       this.$refs.name.focus();
     },
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo;
+    }
   },
 };
 </script>
 
 <style>
-#app {
+#app, .modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -37,6 +70,7 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
 h1 {
   color: rebeccapurple;
   border-bottom: 1px solid #000;
--- modal-project/src/components/Modal.vue
+++ modal-project/src/components/Modal.vue
@@ -1,33 +1,87 @@
 <template>
-    <div class="backdrop">
-        <div class="modal">
-            <p>Modal content</p>
+    <div class="backdrop" @click.self="closeModal">
+        <div class="modal" :class="{ sale: theme === 'sale' }">
+            <slot>default content</slot>
+            <div class="actions">
+                <slot name="links"></slot>
+            </div>
         </div>
     </div>
 </template>
 
-<style>
-    .modal{
-        width: 400px;
-        padding: 20px;
-        margin: 100px auto;
-        background: white;
-        border-radius: 10px;
+<script>
+export default {
+    props: ["theme"],
+    methods: {
+        closeModal() {
+            this.$emit("close");
+        }
     }
-    .backdrop{
-        position: fixed;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        background: rgba(0,0,0,0.5);
-        z-index: 100;
-    }
-    /* .backdrop:target{
-        display: block;
-    } */
-    .backdrop:target .modal{
-        display: block;
-    }
+};
+</script>
 
+<style>
+.modal {
+    width: 400px;
+    padding: 20px;
+    margin: 100px auto;
+    background: white;
+    border-radius: 10px;
+}
+
+.backdrop {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.5);
+    z-index: 100;
+}
+
+.backdrop:target .modal {
+    display: block;
+}
+
+.modal h1 {
+    color: red;
+}
+
+.modal p {
+    font-style: normal;
+}
+
+.modal.sale {
+    background: crimson;
+    color: white;
+}
+
+.modal.sale h1 {
+    color: white;
+    border-bottom: none;
+}
+
+.modal .actions {
+    text-align: center;
+    margin: 30px 0 10px 0;
+    display: flex;
+    justify-content: center;
+}
+
+.modal .actions a {
+    color: #333;
+    padding: 8px;
+    border: 1px solid #eee;
+    border-radius: 4px;
+    text-decoration: none;
+    margin: 10px;
+}
+
+.modal.sell .actions {
+    color: white;
+}
+
+.modal.sell .actions a {
+    color: white;
+}
 </style>
